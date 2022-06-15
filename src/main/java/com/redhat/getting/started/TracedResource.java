0d51a732b815635894ed97c6b9e5359cb2787b08
matteo <mcombi@redhat.com>
2022-06-15 12:04:58 +0200

    added counter

--- src/main/java/com/redhat/getting/started/TracedResource.java
+++ src/main/java/com/redhat/getting/started/TracedResource.java
@@ -2,6 +2,8 @@ package com.redhat.getting.started;
 
 import org.jboss.logging.Logger;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,10 +13,16 @@ import javax.ws.rs.core.MediaType;
 public class TracedResource {
 
     private static final Logger LOG = Logger.getLogger(TracedResource.class);
+    private final MeterRegistry registry;
+
+    TracedResource(MeterRegistry registry) {
+        this.registry = registry;
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        registry.counter("example.hello.count").increment();
         LOG.info("hello");
         return "hello";
     }
