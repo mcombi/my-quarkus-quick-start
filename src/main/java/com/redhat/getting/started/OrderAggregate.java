30639a0a8c5cc7ae3f2e10cf8c930dd6039db968
Matteo <mcombi@redhat.com>
2022-10-10 16:09:27 +0200

    added env variable

--- src/main/java/com/redhat/getting/started/OrderAggregate.java
+++ src/main/java/com/redhat/getting/started/OrderAggregate.java
@@ -1,14 +1,22 @@
 package com.redhat.getting.started;
-
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import java.util.ArrayList;
 
 public class OrderAggregate {
+    @ConfigProperty(name = "qs.customer.name")
+    public String custome;
+    public String customer;
     public long total;
     public ArrayList<Order> orders;
 
     public OrderAggregate(ArrayList<Order> orders){
+        
         this.orders=orders;
-
+        if (custome!=null &&custome.isEmpty()){
+            this.customer="DEFAULT";
+        }
+        else this.customer=custome;
+        
         this.total =orders.stream().count();
 
     }
