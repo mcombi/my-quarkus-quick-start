78e1ed743de0e5edfe461ce2f4680ab1ddecaab0
Matteo <mcombi@redhat.com>
2022-10-11 10:29:17 +0200

    change to cm name

--- src/main/java/com/redhat/getting/started/OrderAggregate.java
+++ src/main/java/com/redhat/getting/started/OrderAggregate.java
@@ -3,7 +3,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import java.util.ArrayList;
 
 public class OrderAggregate {
-    @ConfigProperty(name = "qs.customer.name")
+    @ConfigProperty(name = "cname")
     public String custome;
     public String customer;
     public long total;
